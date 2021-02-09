0d730f3fbf9e3376fa69cd57c3450191d97c6e55
irfansofyana <irfansofyana0305@gmail.com>
2021-02-09 11:46:56 +0700

    add dockerfile

--- sum-service-messaging/client.js
+++ sum-service-messaging/client.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const NATS = require('nats')
 
+console.log(process.env.NATS_URL)
+
 nc = NATS.connect({
-    json: true
+    json: true,
+    url: process.env.NATS_URL
 })
 
 module.exports = nc
